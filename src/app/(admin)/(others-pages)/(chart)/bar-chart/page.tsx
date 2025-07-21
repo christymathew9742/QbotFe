fa2935ae29750bc7e71283567e8dc0f85296370a
mchristy-mufin <m.christy@mufin.co.kr>
2025-07-21 23:42:08 +0530

    space removed

--- src/app/(admin)/(others-pages)/(chart)/bar-chart/page.tsx
+++ src/app/(admin)/(others-pages)/(chart)/bar-chart/page.tsx
@@ -1,6 +1,5 @@
 import BarChartOne from "@/components/charts/bar/BarChartOne";
 import ComponentCard from "@/components/common/ComponentCard";
-// import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import { Metadata } from "next";
 import React from "react";
 
@@ -13,7 +12,6 @@ export const metadata: Metadata = {
 export default function page() {
   return (
     <div>
-      {/* <PageBreadcrumb pageTitle="Bar Chart" /> */}
       <div className="space-y-6">
         <ComponentCard title="Bar Chart 1">
           <BarChartOne />
