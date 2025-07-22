8f5e9c605d931c3a1ed6194036a2ccac52f2d1a5
mchristy-mufin <m.christy@mufin.co.kr>
2025-07-22 21:41:21 +0530

    base url compactibility issues fixed base on the host name

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,2 +1,5 @@
-export const baseURL = 'http://localhost:5001/api'; //https://qbotbackend.onrender.com/
-//export const baseURL = 'https://qbotbackend.onrender.com/api';
+export const baseURL =
+  typeof window !== 'undefined' && window.location.hostname === 'localhost'
+    ? 'http://localhost:5001/api'
+    : 'https://qbotbackend.onrender.com/api';
+
