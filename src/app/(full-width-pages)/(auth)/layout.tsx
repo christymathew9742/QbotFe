520f1b942db226bfa7221e4a3f8d2a5a0648a03c
mchristy-mufin <m.christy@mufin.co.kr>
2025-11-13 22:12:31 +0530

    issues fixed

--- src/app/(full-width-pages)/(auth)/layout.tsx
+++ src/app/(full-width-pages)/(auth)/layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import GridShape from "@/components/common/GridShape";
 import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function AuthLayout({
@@ -11,13 +10,12 @@ export default function AuthLayout({
     <div className="relative p-6 z-1 sm:p-0 dark:bg-gray-900">
       <ThemeProvider>
         <div className="relative flex lg:flex-row w-full h-screen justify-center flex-col  dark:bg-gray-900 sm:p-0">
-          <div className="lg:w-1/2 w-full h-full  dark:bg-white/5 lg:grid items-center hidden">
+          {/* <div className="lg:w-1/2 w-full h-full  dark:bg-white/5 lg:grid items-center hidden">
             <div className="relative items-center justify-center  flex z-1">
-              {/* <GridShape /> */}
               <div className="flex flex-col items-center max-w-xs">
               </div>
             </div>
-          </div>
+          </div> */}
           <div className="w-full sm:basis-3/5 bg-opacity-50 p-8 lg:px-[100px] flex flex-col justify-start overflow-auto relative " >
             <div className="-top-4 left-0 absolute ">
               <img src="images/login/app-logo.png" alt="App Logo " className="w-[30%]" />
