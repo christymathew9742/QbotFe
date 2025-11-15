b134616e99290cc07dff2de1db577b99e5c86f4c
mchristy-mufin <m.christy@mufin.co.kr>
2025-11-15 21:54:43 +0530

    updated new backend1

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,4 @@
 export const baseURL =
   typeof window !== 'undefined' && window.location.hostname === 'localhost'
     ? 'http://localhost:5001/api'
-    : 'https://qbot-backend-india-533766290966.asia-south1.run.app/api';
+    : 'https://qbot-india-533766290966.asia-south1.run.app/api';
