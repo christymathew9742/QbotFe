e8aa4d45af94935c7f2f3615fa831d257473e89d
mchristy-mufin <m.christy@mufin.co.kr>
2025-11-19 12:47:44 +0530

    uploder issues fixed

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,4 @@
 export const baseURL =
   typeof window !== 'undefined' && window.location.hostname === 'localhost'
     ? 'http://localhost:5001/api'
-    : 'https://qbot-india-533766290966.asia-south1.run.app/api';
+    : 'https://qbotbackend-533766290966.asia-south1.run.app/api';
